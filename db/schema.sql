caf48ce3c18eddec2bff22d6ac29fb105f894f51
TheHebi <nturcotte8@live.com>
2022-08-04 15:00:25 -0700

    boilerplate express server setup

---
+++ db/schema.sql
@@ -0,0 +1,2 @@
+DROP DATABASE IF EXISTS rise_db;
+CREATE DATABASE rise_db;
---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const sequelize = require("./config/connection");
+
+// Sets up the Express App
+// =============================================================
+const app = express();
+app.use(timeout('20s'))
+app.use(compression())
+app.use(haltOnTimedout)
+const PORT = process.env.PORT || 3000;
+const allRoutes = require('./controllers');
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use('/',allRoutes);
+
+sequelize.sync({ force: false }).then(function() {
+    app.listen(PORT, function() {
+    console.log('App listening on PORT ' + PORT);
+    });
+});

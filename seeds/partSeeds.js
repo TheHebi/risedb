78c194b7d71298d748b6967885fd83e1742407c1
TheHebi <nturcotte8@live.com>
2022-08-05 12:28:42 -0700

    aknosom seeds

--- seeds/partSeeds.js
+++ seeds/partSeeds.js
@@ -3,13 +3,283 @@ const { Part } = require("../models");
 const partData = [
   {
     MonsterId:1,
+    RankId:1,
+    name: "Aknosom Scale",
+    targetReward: 24,
+    captureReward: 24,
+    brokenPartReward: 30,
+    carve: 43,
+    droppedMaterial:35
+  },
+  {
+    MonsterId:1,
+    RankId:1,
+    name: "Aknosm Feather",
+    targetReward: 32,
+    captureReward: 39,
+    brokenPartReward: 100,
+    carve: 31,
+    droppedMaterial:15
+  },
+  {
+    MonsterId:1,
+    RankId:1,
+    name: "Aknosom Beak",
+    targetReward: 8,
+    captureReward: 12,
+    brokenPartReward: 70,
+    carve: 5,
+    droppedMaterial:0
+  },
+  {
+    MonsterId:1,
+    RankId:1,
+    name: "Flame Sac",
+    targetReward: 24,
+    captureReward: 20,
+    brokenPartReward: 0,
+    carve: 21,
+    droppedMaterial:0
+  },
+  {
+    MonsterId:1,
+    RankId:1,
+    name: "Monster Bone M",
+    targetReward: 12,
+    captureReward: 0,
+    brokenPartReward: 0,
+    carve: 0,
+    droppedMaterial:0
+  },
+  {
+    MonsterId:1,
+    RankId:1,
+    name: "Aknosom Crest",
+    targetReward: 0,
+    captureReward: 5,
+    brokenPartReward: 100,
+    carve: 0,
+    droppedMaterial:0
+  },
+  {
+    MonsterId:1,
+    RankId:1,
+    name: "Wyvern Tear",
+    targetReward: 0,
+    captureReward: 0,
+    brokenPartReward: 0,
+    carve: 0,
+    droppedMaterial:50
+  },
+  {
+    MonsterId:1,
     RankId:2,
     name: "Aknosom Scale+",
     targetReward: 21,
     captureReward: 21,
-    brokenPartReward: 100,
+    brokenPartReward: 25,
     carve: 41,
-    droppedMaterial: 70,
+    droppedMaterial:34
+  },
+  {
+    MonsterId:1,
+    RankId:2,
+    name: "Aknosom Feather+",
+    targetReward: 30,
+    captureReward: 36,
+    brokenPartReward: 100,
+    carve: 29,
+    droppedMaterial:15
+  },
+  {
+    MonsterId:1,
+    RankId:2,
+    name: "Aknosom Beak",
+    targetReward: 12,
+    captureReward: 15,
+    brokenPartReward: 70,
+    carve: 8,
+    droppedMaterial:0
+  },
+  {
+    MonsterId:1,
+    RankId:2,
+    name: "Inferno Sac",
+    targetReward: 24,
+    captureReward: 18,
+    brokenPartReward: 0,
+    carve: 19,
+    droppedMaterial:0
+  },
+  {
+    MonsterId:1,
+    RankId:2,
+    name: "Bird Wyvern Gem",
+    targetReward: 3,
+    captureReward: 5,
+    brokenPartReward: 5,
+    carve: 3,
+    droppedMaterial:1
+  },
+  {
+    MonsterId:1,
+    RankId:2,
+    name: "Monster Keenbone",
+    targetReward: 10,
+    captureReward: 0,
+    brokenPartReward: 0,
+    carve: 0,
+    droppedMaterial:0
+  },
+  {
+    MonsterId:1,
+    RankId:2,
+    name: "Aknosom Crest+",
+    targetReward: 0,
+    captureReward: 5,
+    brokenPartReward: 100,
+    carve: 0,
+    droppedMaterial:0
+  },
+  {
+    MonsterId:1,
+    RankId:2,
+    name: "Wyvern Tear",
+    targetReward: 0,
+    captureReward: 0,
+    brokenPartReward: 0,
+    carve: 0,
+    droppedMaterial:30
+  },
+  {
+    MonsterId:1,
+    RankId:2,
+    name: "Large Wyver Tear",
+    targetReward: 0,
+    captureReward: 0,
+    brokenPartReward: 0,
+    carve: 0,
+    droppedMaterial:20
+  },
+  {
+    MonsterId:1,
+    RankId:3,
+    name: "Aknosom Shard",
+    targetReward: 21,
+    captureReward: 20,
+    brokenPartReward: 25,
+    carve: 39,
+    droppedMaterial:34
+  },
+  {
+    MonsterId:1,
+    RankId:3,
+    name: "Aknosom Plume",
+    targetReward: 30,
+    captureReward: 33,
+    brokenPartReward: 100,
+    carve: 27,
+    droppedMaterial:15
+  },
+  {
+    MonsterId:1,
+    RankId:3,
+    name: "Aknosom Burnbeak",
+    targetReward: 12,
+    captureReward: 15,
+    brokenPartReward: 70,
+    carve: 8,
+    droppedMaterial:0
+  },
+  {
+    MonsterId:1,
+    RankId:3,
+    name: "Conflagrant Sac",
+    targetReward: 24,
+    captureReward: 17,
+    brokenPartReward: 0,
+    carve: 18,
+    droppedMaterial:0
+  },
+  {
+    MonsterId:1,
+    RankId:3,
+    name: "Fey Wyvern Gem",
+    targetReward: 3,
+    captureReward: 4,
+    brokenPartReward: 5,
+    carve: 3,
+    droppedMaterial:1
+  },
+  {
+    MonsterId:1,
+    RankId:3,
+    name: "Monster Toughbone",
+    targetReward: 10,
+    captureReward: 0,
+    brokenPartReward: 0,
+    carve: 0,
+    droppedMaterial:0
+  },
+  {
+    MonsterId:1,
+    RankId:3,
+    name: "Aknosom Head-Crest",
+    targetReward: 0,
+    captureReward: 5,
+    brokenPartReward: 100,
+    carve: 0,
+    droppedMaterial:0
+  },
+  {
+    MonsterId:1,
+    RankId:3,
+    name: "Bird Wyvern Gem",
+    targetReward: 0,
+    captureReward: 6,
+    brokenPartReward: 0,
+    carve: 5,
+    droppedMaterial:0
+  },
+  {
+    MonsterId:1,
+    RankId:3,
+    name: "Wyvern Tear",
+    targetReward: 0,
+    captureReward: 0,
+    brokenPartReward: 0,
+    carve: 0,
+    droppedMaterial:20
+  },
+  {
+    MonsterId:1,
+    RankId:3,
+    name: "Large Wyvern Tear",
+    targetReward: 0,
+    captureReward: 0,
+    brokenPartReward: 0,
+    carve: 0,
+    droppedMaterial:30
+  },
+  {
+    MonsterId:1,
+    RankId:3,
+    name: "Afflicted Monster Bone",
+    targetReward: 0,
+    captureReward: 0,
+    brokenPartReward: 20,
+    carve: 20,
+    droppedMaterial:0
+  },
+  {
+    MonsterId:2,
+    RankId:1,
+    name: "",
+    targetReward: 0,
+    captureReward: 0,
+    brokenPartReward: 0,
+    carve: 0,
+    droppedMaterial:0
   },
 ];
 
